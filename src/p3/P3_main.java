152a05df7ba9e4546ac7ce485d454ec82deaca3f
yelmella <yelmella@smail.uni-koeln.de>
2025-10-30 11:20:21 +0100

    Übung 2

---
+++ src/p3/P3_main.java
@@ -0,0 +1,18 @@
+package p3;
+
+public class P3_main {
+	public static void main(String[] args) {
+		int i=10;
+		int j= 8;
+		int k=12;
+		if(i<j) {
+			System.out.println("A");
+		}
+		if(i<k) {
+			System.out.println("B");
+		}
+		if(j<k) {
+			System.out.println("C");
+		}
+	}
+}

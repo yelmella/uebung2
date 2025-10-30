7b881bf35d52bc66d9328d4b727c6959a3842928
yelmella <yelmella@smail.uni-koeln.de>
2025-10-30 11:13:13 +0100

    Übung 2

---
+++ src/p2/P2_main.java
@@ -0,0 +1,15 @@
+package p2;
+
+public class P2_main {
+	public static void main(String[] args) {
+		int i = 5;
+		boolean b = true;
+		if(i>10) {
+			b = true;
+		} else {
+			b = false;
+		}
+		System.out.println(b); // zur Kontrolle ob true oder false zurückgegeben wird
+	}
+			
+}
